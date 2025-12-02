ed9a92feb20ebbd6bd5591c00c2a31e52beb8fdf
RELIN <regina.lin@snowflake.com>
2025-12-02 23:52:37 +0000

    corrected error

--- sql-pipelines/GOLD/deploy.sql
+++ sql-pipelines/GOLD/deploy.sql
@@ -1,19 +1,15 @@
--- Deploy BRONZE layer
-execute immediate from '../BRONZE/create_customers.sql';
-execute immediate from '../BRONZE/create_orders.sql';
-execute immediate from '../BRONZE/create_part.sql';
-execute immediate from '../BRONZE/create_region.sql'
+BEGIN 
+    -- Deploy BRONZE layer
+    execute immediate from @RL_SQL_DEMO.GOLD.RL_SQL_REPO/branches/main/sql-pipelines/BRONZE/create_customers.sql;
+    execute immediate from @RL_SQL_DEMO.GOLD.RL_SQL_REPO/branches/main/sql-pipelines/BRONZE/create_orders.sql;
+    execute immediate from @RL_SQL_DEMO.GOLD.RL_SQL_REPO/branches/main/sql-pipelines/BRONZE/create_part.sql;
+    
+    -- Deploy SILVER layer
+    execute immediate from @RL_SQL_DEMO.GOLD.RL_SQL_REPO/branches/main/sql-pipelines/SILVER/create_customers_silver.sql;
+    execute immediate from @RL_SQL_DEMO.GOLD.RL_SQL_REPO/branches/main/sql-pipelines/SILVER/create_orders_silver.sql;
+    execute immediate from @RL_SQL_DEMO.GOLD.RL_SQL_REPO/branches/main/sql-pipelines/SILVER/create_part_silver.sql;
 
--- Deploy SILVER layer
-execute immediate from '../SILVER/create_customers_silver.sql';
-execute immediate from '../SILVER/create_orders_silver.sql';
-execute immediate from '../SILVER/create_part_silver.sql';
-execute immediate from '../SILVER/create_region_silver.sql'
+END;
 
--- Deploy GOLD layer
--- (add GOLD objects here when created)
-
--- To deploy all objects via Git repository:
--- ALTER GIT REPOSITORY RL_SQL_DEMO.DEPLOY.RL_SQL_DEMO_GIT_REPO FETCH;
--- execute immediate from @RL_SQL_DEMO.DEPLOY.RL_SQL_DEMO_GIT_REPO/branches/main/sql-pipelines/GOLD/deploy.sql;
+-- execute immediate from @RL_SQL_DEMO.GOLD.RL_SQL_REPO/branches/main/sql-pipelines/GOLD/deploy.sql;
 
